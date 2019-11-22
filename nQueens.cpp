6dc6460b4a356bfe72f50006ade6d923a0383347
Nadinerodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-11-22 18:28:03 -0500

    Update nQueens.cpp

--- nQueens.cpp
+++ nQueens.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+bool ok(int q[], int c) {
+    for(int i = 0; i< c; i++)
+        if(q[i] == q[c] || (c-i) == abs(q[c]-q[i]))
+            return false;
+    return true;
+}
