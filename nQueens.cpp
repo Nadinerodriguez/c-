2eb24c6b8b70def7852266f57b01531dc41a5249
Nadinerodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-11-25 15:52:21 -0500

    Update nQueens.cpp

--- nQueens.cpp
+++ nQueens.cpp
@@ -9,7 +9,30 @@ bool ok(int q[], int c) {
             return false;
     return true;
 }
+int nqueens(int n) {
+  int *q = new int[n];
+  q[0] = 0;   
+  int c = 0;       // Start on the first column
+  int counter=0;
+   while (c >= 0) { // End the loop if you backtrack from the first column
+      c++;
+      if (c == n) { // If you are in the last column, print and backtrack
+         counter++;
+         c--;
+      }
+      else                
+         q[c] = -1;
+      while (c >= 0) {
+         q[c]++;           
+         if (q[c] == n)     // If you have passed the end of the column, backtrack
+            c--;
+         else if (ok(q, c)) break;
+      }
+   }
 
+    delete []q;  // Delete the array.
+      return counter;
+}
 int main() {
     int n;
     cout << "Enter the number of queens you want: ";
