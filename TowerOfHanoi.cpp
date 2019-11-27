9d60868bbed450c431736c183711363a70a1ed5b
Nadinerodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-11-27 01:33:51 -0500

    Update TowerOfHanoi.cpp

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -25,6 +25,33 @@ int main(){
         t[to].push_back(t[from].back());
         t[from].pop_back();
         cout << "Move number " << ++move << ": Transfer ring " << candidate << " from tower " << char(from+65) << " to tower " << char(to+65) << endl;
-  
+         
+        if(n%2 == 1 ) { // Odd number of rings
+            if(t[(to+1)%3].back() < t[(to+2)%3].back())
+                from = (to+1)%3;
+            else
+                from = (to+2)%3;
+            
+            if(t[(from)].back() < t[(from+1)%3].back())
+                to = (from+1)%3;
+            else
+                to = (from+2)%3;
+            // Gets the next candidate
+            candidate = (t[from].back());
+        }
+        else { // Even number of rings
+            if(t[(to+1)%3].back() < t[(to+2)%3].back())
+                from = (to+1)%3;
+            else
+                from = (to+2)%3;
+
+            if(t[(from)].back() < t[(from+2)%3].back())
+                to = (from+2)%3;
+            else
+                to = (from+1)%3;
+            // Gets the next candidate
+            candidate=(t[from].back()); 
+        }
+    }
     return 0;
 }
