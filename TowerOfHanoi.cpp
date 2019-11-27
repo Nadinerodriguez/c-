f710af99930b0676a240c18d858466588c742837
Nadinerodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-11-27 01:27:40 -0500

    Update TowerOfHanoi.cpp

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -9,6 +9,18 @@ int main(){
     cout << "Please enter a number of rings to move: ";
     cin >> n;
     cout << endl;
+    
+    // Intitializes the 3 towers
+    for(int i = n+1; i >= 1; i--)
+        t[0].push_back(i);
+    t[1].push_back(n+1);
+    t[2].push_back(n+1);
+    
+    // Initializes towers and candidate
+    if((n%2) == 1)
+        to = 1;
+    else
+        to = 2;
   
     return 0;
 }
