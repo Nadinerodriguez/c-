e72a509fa1e119369ef106bfea49684e9eacac6a
Nadine Rodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2019-12-30 00:48:38 -0500

    Update rat.cpp

--- rat.cpp
+++ rat.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+class Rat {
+private:
+    int n;
+    int d;
+
+public:
+    Rat() {
+        n = 0;
+        d = 1;
+    }
+    Rat(int i, int j) {
+        n = i;
+        d = j;
+    }
+    Rat(int i) {
+        n = i;
+        d = 1;
+    }
