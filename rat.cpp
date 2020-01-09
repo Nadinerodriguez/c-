78a68ab58fc9bb7b05d67fce678e9655de1a2962
Nadine Rodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2020-01-09 18:51:18 -0500

    Update rat.cpp

--- rat.cpp
+++ rat.cpp
@@ -75,3 +75,37 @@ istream& operator >> (istream& is, Rat& r) {
     is>>r.n>>r.d;
     return is;
 }
+int main() {
+   Rat r1(5, 2), r2(3, 2);
+   
+   cout << "r1: " << r1 << endl;
+   cout << "r2: " << r2 << endl;
+   cout << "r1 + r2: " << r1 + r2 << endl;
+   cout << "r1 - r2: " << r1 - r2 << endl;
+   cout << "r1 * r2: " << r1 * r2 << endl;
+   cout << "r1 / r2: " << r1 / r2 << endl;
+   cout << endl;
+   
+   r1 = r2;
+   r2 = r1 * r2;
+   
+   cout << "r1: " << r1 << endl;
+   cout << "r2: " << r2 << endl;
+   cout << "r1 + r2: " << r1 + r2 << endl;
+   cout << "r1 - r2: " << r1 - r2 << endl;
+   cout << "r1 * r2: " << r1 * r2 << endl;
+   cout << "r1 / r2: " << r1 / r2 << endl;
+   cout << endl;
+   
+   r1 = r2 + r1 * r2 / r1;
+   r2 = r2 + r1 * r2 / r1;
+   
+   cout << "r1: " << r1 << endl;
+   cout << "r2: " << r2 << endl;
+   cout << "r1 + r2: " << r1 + r2 << endl;
+   cout << "r1 - r2: " << r1 - r2 << endl;
+   cout << "r1 * r2: " << r1 * r2 << endl;
+   cout << "r1 / r2: " << r1 / r2 << endl;
+   
+   return 0;
+}
